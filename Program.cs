27da0c229d8c07a479c72e6e2f615dd1b298e5db
Giuseppe <giuseppe.acito.ga@gmail.com>
2022-10-31 16:55:45 +0100

    cercando di capire la logica dei fattoriali e fibonacci

--- Program.cs
+++ Program.cs
@@ -1,95 +1,155 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
 
-//programma
-//stampa
-Console.WriteLine("quanti numeri vuoi nell'array?");
-int numArray = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("quali numeri vuoi nell'array?");
-int[] number = new int[numArray];
-for (int i = 0; i < numArray; i++)
+////programma
+////stampa
+//Console.WriteLine("quanti numeri vuoi nell'array?");
+//int numArray = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("quali numeri vuoi nell'array?");
+//int[] number = new int[numArray];
+//for (int i = 0; i < numArray; i++)
+//{
+//    int numberInArray = Convert.ToInt32(Console.ReadLine());
+//    number[i] = numberInArray;  
+//}
+
+//StampaArray(number);
+//Console.WriteLine();
+
+////quadrato con parametro
+//Quadrato(10);
+
+////quadrato array
+//ElevaArrayAlQuadrato(number);
+//Console.WriteLine("la somma dei numeri dell'array è: " + SommaElementiArray(number));
+
+////somma  array di tutti i numeri e di tutti i numeri di quadrati
+
+
+//SommaElementiArray(number);
+
+
+
+
+
+////funzioni
+////funzione per stampare un array
+//void StampaArray(int[] array)
+//{
+//    for (int i = 0; i < array.Length; i++)
+//    {
+//        Console.Write("elemento " + (i + 1));
+//        Console.WriteLine(" " + array[i]);    
+//    }
+//}
+
+////funzione quadrato
+//int Quadrato(int number)
+//{
+//    int power = number * number;
+//    Console.WriteLine("Il quadrato del numero è: " + power);
+//    return power;
+//}
+////funzione quadrato array
+//int[] ElevaArrayAlQuadrato(int[] array)
+//{
+
+//    int[] newArray = new int[array.Length];
+
+//    for (int i = 0; i < array.Length; i++)
+//    {
+//        int arrPower = array[i] * array[i];
+
+//        newArray[i] = arrPower;
+
+//        Console.WriteLine("potenza di " + array[i] + " è " + newArray[i]);
+
+
+//    }
+
+
+//    Console.WriteLine("la somma delle potenze dei numeri dell'array è: " + SommaElementiArray(newArray));
+
+//    return newArray;
+
+
+
+//}
+
+////funzioni somma array
+
+//int SommaElementiArray(int[] array)
+//{
+
+//    int somma = 0;
+//    for (int i = 0; i < array.Length; i++)
+//    {
+
+//        somma =  array[i] + somma;
+
+//    }
+
+
+//    return somma;
+
+//}
+
+
+
+
+
+/* Si chiede di implementare due funzioni che eseguano le corrsipettive funzioni matematiche:
+    1 - Fattoriale di un numero
+    2 - La sequenza di fibonacci di un numero
+    1) il fattoriale di un numero può essere calcolato solo su numeri positivi:
+0! = 1
+1! = 1
+2! = 2
+3! = 6
+4! = 24
+5! = 120
+N! = ???
+    risolverla in modo RICORSIVO
+    o anche in modo ITERATIVO (FOR)
+        n	Fn
+        0	0
+        1	1
+        2	1
+        3	2
+        4	3
+        5	5
+        6	8
+        7	13
+        8	21
+        9	34
+        10	55
+fibonacci(10) = 55
+*/
+
+
+//funzione fattoriale il fattoriale è la moltiplicazione decrescente di un num es. 3! = 3x2x1 = 6
+
+//FunctionFibonacciNumber();
+factorial(5);
+
+int factorial(int number)      
 {
-    int numberInArray = Convert.ToInt32(Console.ReadLine());
-    number[i] = numberInArray;  
-}
-
-StampaArray(number);
-Console.WriteLine();
-
-//quadrato con parametro
-Quadrato(10);
-
-//quadrato array
-ElevaArrayAlQuadrato(number);
-Console.WriteLine("la somma dei numeri dell'array è: " + SommaElementiArray(number));
-
-//somma  array di tutti i numeri e di tutti i numeri di quadrati
-
-
-SommaElementiArray(number);
-
-
-
-
-
-//funzioni
-//funzione per stampare un array
-void StampaArray(int[] array)
-{
-    for (int i = 0; i < array.Length; i++)
+    int calcolo = 0;
+    if (number > 0 && number <= 10)
     {
-        Console.Write("elemento " + (i + 1));
-        Console.WriteLine(" " + array[i]);    
+        calcolo = number * factorial(number - 1);
     }
+
+    Console.WriteLine(calcolo);
+    return calcolo;
+    
 }
 
-//funzione quadrato
-int Quadrato(int number)
-{
-    int power = number * number;
-    Console.WriteLine("Il quadrato del numero è: " + power);
-    return power;
-}
-//funzione quadrato array
-int[] ElevaArrayAlQuadrato(int[] array)
-{
-
-    int[] newArray = new int[array.Length];
-
-    for (int i = 0; i < array.Length; i++)
-    {
-        int arrPower = array[i] * array[i];
-        
-        newArray[i] = arrPower;
-
-        Console.WriteLine("potenza di " + array[i] + " è " + newArray[i]);
 
 
-    }
-    
-
-    Console.WriteLine("la somma delle potenze dei numeri dell'array è: " + SommaElementiArray(newArray));
-    
-    return newArray;
-    
 
 
-}
-
-//funzioni somma array
-
-int SommaElementiArray(int[] array)
-{
-    
-    int somma = 0;
-    for (int i = 0; i < array.Length; i++)
-    {
-        
-        somma =  array[i] + somma;
-              
-    }
-    
-    
-    return somma;
-
-}
+//int[] FunctionFibonacciNumber(int[] array)
+//{
+//    return array;
+//}
